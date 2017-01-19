9ae606dbdfa00e9216ae81d22ed3ced457b54ea6
Andrew Nabok <andrewnabok@MacBook-Pro-Andrew.local>
2017-01-19 18:09:26 +0200

    js edited 2

--- js1/js/script.js
+++ js1/js/script.js
@@ -1,3 +1,4 @@
+/*// рекурсія
 // Просимо користувача ввести число та степінь
 var base = prompt('Введіть число', '');
 var degree = prompt('Введіть степінь', '');
@@ -12,4 +13,30 @@ function func(x, n) {
   }
 }
 
-console.log(func(base, degree));
+console.log(func(base, degree));*/
+var base = prompt('Введіть число', '');
+var degree = prompt('Введіть степінь', '');
+
+function pow(a, n) {
+
+    if (n == 0 && a > 0) {
+        return 1;
+    } else if (n == 0 && a < 0) {
+        return -1;
+    } else if (n > 0) {
+        var b = a;
+        for (var i = 1; i < n; i++) {
+            b *= a;
+        }
+        return b;
+    } else if (n < 0) {
+        var c = a;
+        for (var j = 0; j >= n; j--) {
+            c *= (10 / a);
+            c /= 10;
+        }
+        return c;
+    }
+}
+
+console.log(pow(base, degree));
